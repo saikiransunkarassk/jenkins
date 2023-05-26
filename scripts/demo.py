859bf8c0ab8c369b66e12fe13d011253bec948ec
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-26 11:55:20 +0530

    hello

--- scripts/demo.py
+++ scripts/demo.py
@@ -4,4 +4,5 @@ import os
 os.environ["NEW_ENV1"]="new environmental variable is set"
 
 commandList=["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"]
+print(os.environ)
 subprocess.run(commandList)
