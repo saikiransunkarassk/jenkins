68ef5b8db11e392c77e9e51c1779e030b887bcc9
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-30 13:19:58 +0530

    hello

--- scripts/demo.py
+++ scripts/demo.py
@@ -11,7 +11,7 @@ args=parser.parse_args()
 print(args)
 if(args.changeVar[0] in os.environ.keys()):
     # os.environ[args.changeVar[0]]=args.changeVar[1] #temp change
-    subprocess.run(["export ",args.changeVar[0],"=",args.changeVar[1]])
+    subprocess.run(["export ",args.changeVar[0],"=\"",args.changeVar[1]+"\""])
     print("environment varable successfully changed")
 
 # os.environ["NEW_VAR"]="Hello World!" #temp storage
