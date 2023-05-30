ffd9784baa71aa323cf2d686cbddd4a8d5a5f22f
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-30 13:36:36 +0530

    hello

--- scripts/demo.py
+++ scripts/demo.py
@@ -6,20 +6,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--changeVar",nargs=2)
 
+
 args=parser.parse_args()
 
-print(args)
-   
+subprocess.Popen('export {val1}="{val2}"'.format(val1=args.changeVar[0],val2=args.changeVar[1]), shell=True).wait()
 
-with open(os.path.expanduser("~/.bashrc"), "a") as outfile:
-    if(args.changeVar[0] in os.environ.keys()):
-        outfile.write("export "+args.changeVar[0]+"="+"\""+args.changeVar[1]+"\"")
-        print("environment varable successfully changed")
-    
-    
-
-
-print("new environment variable is created")
+if(args.changeVar[0] not in os.environ.keys()):  
+    print("new environment variable is created")
 
 val=subprocess.run(["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"],capture_output=True,text=True)
 
