7d6b74e3e08a311432d675997d7a2b897dd22ea6
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-30 17:34:52 +0530

    hello

--- scripts/main.py
+++ scripts/main.py
@@ -8,19 +8,15 @@ parser.add_argument("--changeVar",nargs=2)
 
 args=parser.parse_args()
 
-os.system("export {0}={1}".format(args.changeVar[0], args.changeVar[1]))
-os.system("echo \"export {0}={1}\" >> ~/.bashrc".format(args.changeVar[0], args.changeVar[1]))
+val=subprocess.run(["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"],capture_output=True,text=True)
 
-os.system("export NEW_VAR=\"HELLO\"")
-os.system("echo \"export NEW_VAR=HELLO\" >> ~/.bashrc")
+os.environ[args.changeVar[0]]=args.changeVar[1]
 
-
-print(os.environ["ENV_VALUE"])
+os.environ["NEW_VAR"]="hello"
 
 if(args.changeVar[0] not in os.environ.keys()):  
     print("new environment variable is created")
 
-val=subprocess.run(["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"],capture_output=True,text=True)
 
 output=val.stdout
 
