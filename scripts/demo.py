35c1cc16473a8015e17e8d55a8588ec3a3bea088
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-30 14:24:34 +0530

    hello

--- scripts/demo.py
+++ scripts/demo.py
@@ -9,9 +9,8 @@ parser.add_argument("--changeVar",nargs=2)
 
 args=parser.parse_args()
 
-with open(os.path.expanduser("~/.bashrc"), "a") as outfile:
-    outfile.write('export {val1}="{val2}"'.format(val1=args.changeVar[0],val2=args.changeVar[1]))
-    
+print(os.environ["ENV_VALUE"])
+
 if(args.changeVar[0] not in os.environ.keys()):  
     print("new environment variable is created")
 
