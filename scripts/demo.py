be8ff3cba455b673d30be31d2c8c2a5f16e54e9b
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-05-30 13:11:23 +0530

    hello

--- scripts/demo.py
+++ scripts/demo.py
@@ -11,22 +11,16 @@ args=parser.parse_args()
 print(args)
 if(args.changeVar[0] in os.environ.keys()):
     # os.environ[args.changeVar[0]]=args.changeVar[1] #temp change
-    subprocess.run(f"export ${args.changeVar[0]}=${args.changeVar[1]}",shell=True)
+    subprocess.run(["export ",args.changeVar[0],"=",args.changeVar[1]],shell=True)
     print("environment varable successfully changed")
 
 # os.environ["NEW_VAR"]="Hello World!" #temp storage
 
-subprocess.run(f"export ${args.changeVar[0]}=${args.changeVar[1]}")
+subprocess.run(["export ",args.changeVar[0],"=",args.changeVar[1]],shell=True)
 
 print("new environment variable is created")
 
-commandList1=["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"]
-
-commandList2=["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"]
-
-val=subprocess.run(commandList1,capture_output=True,text=True)
-
-
+val=subprocess.run(["conftest","verify", "--policy" ,"./policies/policy1", "--output=table"],capture_output=True,text=True)
 
 output=val.stdout
 
