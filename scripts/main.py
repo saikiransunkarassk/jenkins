961cb326f1f719ee1a79f16d5a4b59b1ef7f4087
saikiransunkarassk <sunkara.saikiran.nrp@gmail.com>
2023-06-01 15:41:03 +0530

    hello

--- scripts/main.py
+++ scripts/main.py
@@ -54,6 +54,8 @@ class NewClass:
 
 if __name__ == "__main__":
 
+    f = open("tempFile", "w+")
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--location")
@@ -81,15 +83,15 @@ if __name__ == "__main__":
 
         newObject.printEnvVar()
 
-    # if (inputs.changeEnvVal != None):
+    if (inputs.changeEnvVal != None):
 
-    #     f.write(f"c ${inputs.changeEnvVal[0]} ${inputs.changeEnvVal[1]}\n")
+        f.write(f"c ${inputs.changeEnvVal[0]} ${inputs.changeEnvVal[1]}\n")
 
-    # if (inputs.addEnvVar != None):
+    if (inputs.addEnvVar != None):
 
-    #     f.writelines(f"a ${inputs.addEnvVar[0]} ${inputs.addEnvVar[1]}")
+        f.write(f"a ${inputs.addEnvVar[0]} ${inputs.addEnvVar[1]}")
 
-    # f.close()
+    f.close()
 
     if (newObject.returnCode == True):
         print("successful")
